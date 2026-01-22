94a51d078079bc48e8c19af4b6abc1a1ffd1586b
Caio Aloisio <caio.aloisio@outlook.com>
2026-01-21 21:49:36 -0300

    Setup inicial do projeto

---
+++ carregar_dataset.py
@@ -0,0 +1,14 @@
+from ucimlrepo import fetch_ucirepo 
+  
+# fetch dataset 
+predict_students_dropout_and_academic_success = fetch_ucirepo(id=697) 
+  
+# data (as pandas dataframes) 
+X = predict_students_dropout_and_academic_success.data.features 
+y = predict_students_dropout_and_academic_success.data.targets 
+  
+# metadata 
+print(predict_students_dropout_and_academic_success.metadata) 
+  
+# variable information 
+print(predict_students_dropout_and_academic_success.variables) 
